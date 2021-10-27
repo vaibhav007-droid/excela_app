1257f63cf6b59d41a0fbe4cbd0f47e8fea0d79bf
Vaibhav007-droid <parmarvaibhav773@gmail.com>
2021-10-27 17:56:57 +0530

    Initial commit

---
+++ assets/url-constants/url-constants.spec.ts
@@ -0,0 +1,7 @@
+import { UrlConstants } from '../url-constants/url-constants';
+
+describe('UrlConstants', () => {
+  it('should create an instance', () => {
+    expect(new UrlConstants()).toBeTruthy();
+  });
+});
---
+++ assets/url-constants/url-constants.ts
@@ -0,0 +1,18 @@
+export class UrlConstants {
+    public static SIGNUP_API='/excela.excela.api.excela_api.user_sign_up'
+    public static COUNTRY_LIST_API='/excela.excela.api.excela_api.country'
+    public static LOGIN_API='/excela.excela.api.excela_api.user_login'
+    public static RESET_PASSWORD_API='/excela.excela.api.excela_api.forgot_password'
+    public static JOB_INTEREST_API='/excela.excela.api.excela_api.job_seeker_resume'
+    public static GET_JOBS_API='/excela.excela.api.excela_api.jobs'
+    public static SEARCH_JOBS_API='/excela.excela.api.excela_api.job_search'
+    public static GET_JOB_DETAILS_API='/excela.excela.api.excela_api.get_job_details'
+    public static GET_JOB_STATUS_API='/excela.excela.api.excela_api.validate_job_applicantion'
+    public static GET_APPLICATION_STATUS_API='/excela.excela.api.excela_api.applicant_status'
+    public static UPDATE_PROFIL_EXPERIENCE_API='/excela.excela.api.excela_api.get_experience'
+    public static UPDATE_PROFIL_FRESHER_API='/excela.excela.api.excela_api.get_fresher'
+    public static UPLOAD_DOCUMENT_API='/excela.excela.api.excela_api.upload_documents'
+    public static DELETE_DOCUMENT_API='/excela.excela.api.excela_api.get_delete'
+    public static GET_UPLOADED_DOCUMENTS_API='/excela.excela.api.excela_api.get_documents'
+
+}
